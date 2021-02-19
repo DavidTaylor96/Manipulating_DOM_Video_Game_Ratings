aaea2eb4cffa368c9a58d0b602690a0c46de26c0
DavidTaylor96 <david_taylor96@outlook.com>
2021-02-19 17:07:25 +0000

    extentions

--- js/app.js
+++ js/app.js
@@ -6,19 +6,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const deleteAll = document.querySelector('#delete-all')
   deleteAll.addEventListener('click', handleDeleteAll)
 
-  // const body = document.querySelector('body')
-  // const createButton = document.querySelector('#create-button')
-  // createButton.addEventListener('click', () => {
-  //   createNewButton(body)
-  // })
+  const body = document.querySelector('game-list-item')
+  const createButton = document.querySelector('#create-button')
+  createButton.addEventListener('click', () => {
+    handleSubmitForm(body)
+  })
 })
 
 
 const handleSubmitForm = function (evt) {
   evt.preventDefault();
+
   const resultParagraph = document.querySelector('#game-list')
   const createGame = document.createElement('li')
-  createGame.textContent = `Name: ${evt.target.name.value}, Genre: ${evt.target.genre.value}, Ratings: ${evt.target.rating.value}`
+  createGame.classList.add('game-list-item')
+
+  createGame.textContent = `Name: ${evt.target.name.value}, Genre: ${evt.target.genre.value} Ratings: ${evt.target.rating.value}`
+
   resultParagraph.appendChild(createGame)
 
   document.getElementById('new-items').reset();
@@ -30,10 +34,10 @@ const handleDeleteAll = function (evt) {
   gameList.textContent = ''
 }
 
-// const createNewButton = (parentElement) => {
-//   const newButton = document.createElement('button')
-//   newButton.textContent = "Delete"
-//   parentElement.appendChild(newButton)
-//   console.log(newButton)
-//   console.log(parentElement)
-// }
+const createNewButton = (parentElement) => {
+  const newButton = document.createElement('button')
+  newButton.textContent = "Delete"
+  parentElement.appendChild(newButton)
+  console.log(newButton)
+  console.log(parentElement)
+}
